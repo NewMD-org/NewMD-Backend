1fa771f091f350a36bf09dc0c754468bd0558845
LAZPbanahaker(Andy) <pn0818x@outlook.com>
2022-01-27 21:21:10 +0800

    Change API router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const index = require('./router/index');
+const API = require('./router/API');
 
-app.use('/', index);
+app.use('/API/', API);
 
 module.exports = app;
