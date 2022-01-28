5b128c558762fc7f43a183a0555804ed81a3f6a0
LAZPbanahaker(Andy) <pn0818x@outlook.com>
2022-01-28 20:41:30 +0800

    cors config

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const API = require('./router/API');
+
+app.use(cors());
 
 app.use('/API/', API);
 
--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,7 @@
 const app = require('../app');
-const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.set('port', port);
-app.use(cors());
 
 const server = app.listen(app.get('port'), () => {
     console.log('server is running on http://localhost:3000');
