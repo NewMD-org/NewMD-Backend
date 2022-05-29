d997d1e76b426345a8df17c47436b3a4da4fd94f
banahaker <pn0818x@outlook.com>
2022-05-29 22:40:46 +0800

    fix: delete LRU cache

--- controllers/API.js
+++ controllers/API.js
@@ -1,32 +1,34 @@
 import getSchedule from '../function/memoryTable.js';
-import { CGet } from '../lib/LRUCache.js';
 
 export const onlyClassName = async (req, res) => {
     const params = req.params;
     const key = params.className;
-    await CGet(key, () => {
-        return getSchedule(params.className);
-    }).then(data => {
+    const data = await getSchedule(params.className);
+    if( data.error ) {
+        res.status(400).json(data);
+    }else {
         res.status(200).json(data);
-    });
+    }
 };
 
 export const classNameWithYear = async (req, res) => {
     const params = req.params;
     const key = params.className + params.year;
-    await CGet(key, () => {
-        return getSchedule(params.className, params.year);
-    }).then(data => {
+    const data = await getSchedule(params.className, params.year);
+    if( data.error ) {
+        res.status(400).json(data);
+    }else {
         res.status(200).json(data);
-    });
+    }
 };
 
 export const allParams = async (req, res) => {
     const params = req.params;
     const key = params.className + params.year + params.week;
-    await CGet(key, () => {
-        return getSchedule(params.className, Number(params.year), Number(params.week));
-    }).then(data => {
+    const data = getSchedule(params.className, Number(params.year), Number(params.week));
+    if( data.error ) {
+        res.status(400).json(data);
+    }else {
         res.status(200).json(data);
-    });
+    }
 };
