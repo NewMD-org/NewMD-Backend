e5e44c5c2a55a776b313b6a434f953dc22a14fef
OnCloud <anonymousaaaa41414141@gmail.com>
2023-03-25 23:41:46 +0800

    Fix code style

--- _modules/TWtime/index.js
+++ _modules/TWtime/index.js
@@ -1,11 +1,11 @@
 export function TWtime() {
     let dateObject_TW = new Date();
-    let timeString_TW = dateObject_TW.toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' });
-    let millisecondString_TW = dateObject_TW.getMilliseconds().toLocaleString('zh-TW');
+    let timeString_TW = dateObject_TW.toLocaleString("zh-TW", { timeZone: "Asia/Taipei" });
+    let millisecondString_TW = dateObject_TW.getMilliseconds().toLocaleString("zh-TW");
     return {
-        full: timeString_TW + ':' + ("000" + millisecondString_TW).slice(-3) + ' (GMT+8)',
+        full: timeString_TW + ":" + ("000" + millisecondString_TW).slice(-3) + " (GMT+8)",
         time: timeString_TW,
-        gmt: '(GMT+8)',
-        timeZone: 'Asia/Taipei'
+        gmt: "(GMT+8)",
+        timeZone: "Asia/Taipei"
     };
 }
