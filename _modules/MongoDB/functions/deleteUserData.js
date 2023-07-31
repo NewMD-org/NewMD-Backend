2c9255dba67f819640f7058ea1cf30bddf8e462c
OnCloud <anonymousaaaa41414141@gmail.com>
2023-08-01 02:08:15 +0800

    Add JSDoc

--- _modules/MongoDB/functions/deleteUserData.js
+++ _modules/MongoDB/functions/deleteUserData.js
@@ -1,6 +1,27 @@
 import { schema_userData } from "./mongo-schema.js";
 
 
+/**
+ * Delete user data from the database.
+ *
+ * @param {string} ID - The user ID.
+ * @param {string} PWD - The user password.
+ * @returns {Promise<number>} A Promise that resolves to an integer indicating the operation result:
+ * - 1: The user data was successfully deleted.
+ * @throws {Error} If there was an error while executing the database operations or if the user data was not found or failed to delete.
+ * @async
+ * @example
+ * const ID = 'username';
+ * const PWD = 'password';
+ *
+ * try {
+ *   const result = await deleteUserData(ID, PWD);
+ *   console.log(result); // 1
+ * } catch (error) {
+ *   console.error(error.message);
+ * }
+ */
+
 export default async function deleteUserData(ID, PWD) {
     try {
         const data = await schema_userData.findOne({ userID: ID, userPassword: PWD }).exec();
--- _modules/MongoDB/functions/saveUserData.js
+++ _modules/MongoDB/functions/saveUserData.js
@@ -1,8 +1,41 @@
 import { schema_userData } from "./mongo-schema.js";
 
 
+/**
+ * Store user data in the database or update existing user data.
+ *
+ * @param {string} ID - The user ID.
+ * @param {string} PWD - The user password.
+ * @param {Object} dataToSave - The data to store or update for the user.
+ * @param {string} dataToSave.userID - The user ID.
+ * @param {string} dataToSave.userPassword - The user password.
+ * @param {string} dataToSave.year - The year associated with the user data.
+ * @param {Object} dataToSave.table - The table data associated with the user data.
+ * @param {Date} dataToSave.updatedAt - The update timestamp associated with the user data.
+ * @returns {Promise<number>} A Promise that resolves to an integer indicating the operation result:
+ * - 0: The data was successfully stored for a new user.
+ * - 1: The data was successfully updated for an existing user.
+ * @throws {Error} If there was an error while executing the database operations or failed to authorize the user.
+ * @async
+ * @example
+ * const ID = 'username';
+ * const PWD = 'password';
+ * const dataToSave = {
+ *   userID: 'username',
+ *   userPassword: 'password',
+ *   year: '1123',
+ *   table: { ... },
+ *   updatedAt: new Date()
+ * };
+ *
+ * try {
+ *   const result = await storeUserData(ID, PWD, dataToSave);
+ *   console.log(result); // 0 or 1
+ * } catch (error) {
+ *   console.error(error.message);
+ * }
+ */
 export default async function storeUserData(ID, PWD, dataToSave) {
-    console.log(dataToSave);
     try {
         const data = await schema_userData.findOne({ userID: ID, userPassword: PWD }).exec();
         if (data == null) {
