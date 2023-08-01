0f59382878e45c2c645958f435b9fac601d981d1
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-08-01 09:46:39 +0800

    Add new method to get table

--- _modules/MdTimetableAPI/functions/getTable.js
+++ _modules/MdTimetableAPI/functions/getTable.js
@@ -4,46 +4,57 @@ import iconv from "iconv-lite";
 
 export async function getTable(ID, PWD, timeout) {
     try {
-        const getCookie = await axios.get(
-            "http://140.128.156.92/AACourses/Web/wLogin.php",
-            {
-                validateStatus: (status) => {
-                    return status === 200;
-                }
-            }
-        );
-        const cookie = getCookie.headers["set-cookie"][0].split(";")[0];
-
-        const getTableResponse = await axios.postForm(
-            "http://140.128.156.92/AACourses/Web/wLogin.php",
-            {
-                sureReg: "YES",
-                accessWay: "ACCOUNT",
-                wRole: "STD",
-                stdID: ID,
-                stdPWD: PWD
-            },
-            {
-                headers: {
-                    cookie: cookie
-                },
-                validateStatus: (status) => {
-                    return status === 200;
-                },
-                timeout: timeout,
-                responseType: "arraybuffer",
-                transformResponse: [data => {
-                    // eslint-disable-next-line no-undef
-                    return iconv.decode(Buffer.from(data), "big5");
-                }]
-            }
-        );
-
-        return {
-            tableHTML: getTableResponse.data,
-            cookie: cookie
-        };
+        const account = { ID, PWD };
+        try {
+            return await ClassArrangementSystemLogin(account, timeout, "http://140.128.156.92/AACourses/Web/wLogin.php");
+        } catch (error) {
+            return await ClassArrangementSystemLogin(account, timeout, "https://s44.mingdao.edu.tw/AACourses/Web/wLogin.php");
+        }
     } catch (error) {
         throw new Error("fastTable : MD server error");
     }
+}
+
+async function ClassArrangementSystemLogin(accountData, timeout, URL) {
+    const { ID, PWD } = accountData;
+
+    const getCookie = await axios.get(
+        URL,
+        {
+            validateStatus: (status) => {
+                return status === 200;
+            }
+        }
+    );
+    const cookie = getCookie.headers["set-cookie"][0].split(";")[0];
+
+    const getTableResponse = await axios.postForm(
+        URL,
+        {
+            sureReg: "YES",
+            accessWay: "ACCOUNT",
+            wRole: "STD",
+            stdID: ID,
+            stdPWD: PWD
+        },
+        {
+            headers: {
+                cookie: cookie
+            },
+            validateStatus: (status) => {
+                return status === 200;
+            },
+            timeout: timeout,
+            responseType: "arraybuffer",
+            transformResponse: [data => {
+                // eslint-disable-next-line no-undef
+                return iconv.decode(Buffer.from(data), "big5");
+            }]
+        }
+    );
+
+    return {
+        tableHTML: getTableResponse.data,
+        cookie: cookie
+    };
 }
